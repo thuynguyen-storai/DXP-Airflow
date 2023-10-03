7471f3e5e48fd3f29cc5c6c29fd585a5805aa7d1
thuynguyen-storai <thuy.n@stor.ai>
2023-10-03 15:41:58 +0700

    (v1.0.1) fix: Airflow not render `trigger_adf_dag`

--- airflow/dags/trigger_adf_dag.py
+++ airflow/dags/trigger_adf_dag.py
@@ -11,6 +11,7 @@ def trigger_adf_dag():
         task_id="trigger_adf_dag", azure_data_factory_conn_id="adf_conn", pipeline_name="test_airflow"
     )
 
+trigger_adf_dag()
 
 if __name__ == "__main__":
     trigger_adf_dag().test()
